afe8455d8cd5d20605692e3522856a00767296e6
aleks <aleks@DESKTOP-1MKA3C2>
2024-02-25 15:15:18 +0100

    Add project files.

---
+++ ProjObj1/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+using PROJOBJ1;
+using System.Text.Json;
+
+
+public class Program
+{
+    public static int Main(string[] args)
+    {
+        
+        string InPath = "example_data.ftr";
+        string OutPath = "kokskoks.json";
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Argumenty: [string] input file path, [string] output file path");
+            return -1;
+        }
+        else
+        {
+            InPath= args[0];
+            OutPath= args[1];
+        }
+        //Dictionary<string,Factory> dic = new Dictionary<string, Factory>();
+        //dic.Add("CA", new CargoFactory());
+        //dic.Add("C", new CrewFactory());
+        //dic.Add("P", new PassengerFactory());
+        //dic.Add("CP", new CargoPlaneFactory());
+        //dic.Add("PP", new PassengerPlaneFactory());
+        //dic.Add("AI", new AirportFactory());
+        //dic.Add("FL",new FlightFactory());
+        LoadUtil util=new LoadUtil();
+        List<AbstractProduct> list=util.LoadObjects(InPath);
+        LoadUtil.SerializeList(list, OutPath);
+        return 0;
+        
+    }
+}
